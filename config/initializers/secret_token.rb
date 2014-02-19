78d2395b37f96bbb448ff3cdcb14ee022ad2a64e
sai <sai@sai-300E4Z-300E5Z-300E7Z.(none)>
2014-02-19 12:13:19 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seema::Application.config.secret_token = 'ba8a8ec84f4dc8f203935da25f64f0f51540cddf88c216fcb79eb9f9a7f15247de1e262be4ce65df472abcb1b05f6dd87a99f49a8d6258023d5580e594cc07eb'
